13b89d4234a065966eefd416f19b9b6e2318c3cf
brendan gilroy <bdgilroy@gmail.com>
2015-05-18 19:44:04 -0400

    made amazon api wrapper work

--- project/client/word_wrapper.py
+++ project/client/word_wrapper.py
@@ -1,5 +1,6 @@
 import random
-from random import randint
+import os
+
 
 class RandWord:
     def __init__(self):
@@ -7,18 +8,20 @@ class RandWord:
 
     def get_random_word(self):
         word_array = []
-        word_array = [line.strip() for line in open('words','r')]
+        word_array = [line.strip() for line in open('client/words','r')]
         # for word in word_array:
         #     print(word)
         # print()
         random_word = random.choice(word_array)
-        # print(random_word)
         return(random_word)
 
         # for line in open('words'):
         #     lines = lines + 1
         # print(lines)
         # print(randint(1, lines))
+if __name__ == '__main__':
+    retval = os.getcwd()
+    print(retval)
 
-# rand = RandWord()
-# rand.get_random_word()
+    rand = RandWord()
+    rand.get_random_word()
